a1cf443c3033914a0abb754df00237d1ca35e32e
Philipp Meister <philipp.meister@hacon.de>
2024-07-31 15:20:13 +0200

    blame + toggle between source and header file

--- init.lua
+++ init.lua
@@ -240,6 +240,13 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  {
+    'FabijanZulj/blame.nvim',
+    config = function()
+      require('blame').setup()
+    end,
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
@@ -520,6 +527,7 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader><Tab>', '<cmd>ClangdSwitchSourceHeader<CR>', 'switch between source and header')
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
