28cfe6e2eae5e1c988e1da3ff87d4a56c2c836d4
Philipp Meister <philipp.meister@hacon.de>
2025-05-13 08:33:28 +0200

    summer-time

--- init.lua
+++ init.lua
@@ -875,7 +875,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.o.background = 'light'
+      -- vim.o.background = 'light'
       vim.cmd.colorscheme 'default'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
