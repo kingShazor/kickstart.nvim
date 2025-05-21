b2eef82f1576191770f6d7be18ad48842bf6f08e
Philipp Meister <philipp.meister@hacon.de>
2025-05-21 10:47:43 +0200

    clangd fix

--- init.lua
+++ init.lua
@@ -333,6 +333,7 @@ require('lazy').setup({
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
     branch = '0.1.x',
+    -- branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -646,7 +647,7 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          map('<leader><Tab>', '<cmd>ClangdSwitchSourceHeader<CR>', 'switch between source and header')
+          map('<leader><Tab>', '<cmd>LspClangdSwitchSourceHeader<CR>', 'switch between source and header')
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
