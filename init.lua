274981aaf3e82087bd99ef87a7887f8b67492602
Philipp Meister <philipp.meister@hacon.de>
2025-05-23 11:04:16 +0200

    fix lua-list

--- init.lua
+++ init.lua
@@ -238,7 +238,7 @@ vim.lsp.config.luals = {
   -- ".luarc.jsonc" file. Files that share a root directory will reuse
   -- the connection to the same LSP server.
   -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
   -- Specific settings to send to the server. The schema for this is
   -- defined by the server. For example the schema for lua-language-server
   -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
