33bd1ae3d0dc9b81ddc4ba78e86d18704292cad0
kingShazor <constantin.p.meister@gmail.com>
2025-06-22 21:25:38 +0200

    Zettel-kasten + Markdown-rendering

--- init.lua
+++ init.lua
@@ -331,6 +331,23 @@ require('lazy').setup({
     },
   },
   {
+    'zk-org/zk-nvim',
+    config = function()
+      require('zk').setup {
+        -- See Setup section below
+      }
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
