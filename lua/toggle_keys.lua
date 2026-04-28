05ddc9177cf81318695bfab4cb4c94823c3196c7
kingShazor <175230427+kingShazor@users.noreply.github.com>
2026-04-28 22:32:01 +0200

    toggle_keys format - also use {} in c-mode

--- lua/toggle_keys.lua
+++ lua/toggle_keys.lua
@@ -1,4 +1,4 @@
-M = { }
+M = {}
 
 M.coding = true
 
@@ -7,29 +7,21 @@ M.toggle_keys = function()
 end
 
 M.initKeyMaps = function()
-  vim.keymap.set('i', 'ö', function()
+  vim.keymap.set({ 'i', 'n', 'o' }, 'ö', function()
     return require('toggle_keys').coding and '[' or 'ö'
   end, { expr = true, desc = 'coding "["' })
 
-  vim.keymap.set('i', 'Ö', function()
+  vim.keymap.set({ 'i', 'n', 'c' }, 'Ö', function()
     return require('toggle_keys').coding and '{' or 'Ö'
   end, { expr = true, desc = 'coding "{"' })
 
-  vim.keymap.set('i', 'ä', function()
+  vim.keymap.set({ 'i', 'n', 'o' }, 'ä', function()
     return require('toggle_keys').coding and ']' or 'ä'
   end, { expr = true, desc = 'coding "]"' })
 
-  vim.keymap.set('i', 'Ä', function()
+  vim.keymap.set({ 'i', 'n', 'c' }, 'Ä', function()
     return require('toggle_keys').coding and '}' or 'Ä'
   end, { expr = true, desc = 'coding "}"' })
-
-  vim.keymap.set('n', 'ö', function()
-    vim.api.nvim_feedkeys(']', 'i', false)
-  end, { expr = true, desc = '"]"' })
-
-  vim.keymap.set('n', 'ä', function()
-    vim.api.nvim_feedkeys('[', 'i', false)
-  end, { expr = true, desc = '"]"' })
 
   vim.keymap.set('n', '<leader>t', function()
     require('toggle_keys').toggle_keys()
