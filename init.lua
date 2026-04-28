2ba3cb8e7000b42e499dae428a3f92bce3ba3d63
kingShazor <175230427+kingShazor@users.noreply.github.com>
2026-04-28 20:22:07 +0200

    outsourced toggle keys vim.keymap.set

--- init.lua
+++ init.lua
@@ -345,33 +345,7 @@ vim.schedule(function()
   local wk = require 'which-key'
   wk.setup()
 
-  vim.keymap.set('i', 'ö', function()
-    return require('toggle_keys').coding and '[' or 'ö'
-  end, { expr = true, desc = 'coding "["' })
-
-  vim.keymap.set('i', 'Ö', function()
-    return require('toggle_keys').coding and '{' or 'Ö'
-  end, { expr = true, desc = 'coding "{"' })
-
-  vim.keymap.set('i', 'ä', function()
-    return require('toggle_keys').coding and ']' or 'ä'
-  end, { expr = true, desc = 'coding "]"' })
-
-  vim.keymap.set('i', 'Ä', function()
-    return require('toggle_keys').coding and '}' or 'Ä'
-  end, { expr = true, desc = 'coding "}"' })
-
-  vim.keymap.set('n', '<leader>t', function()
-    require('toggle_keys').toggle_keys()
-  end, { desc = 'toggle between coding and german writing keys' })
-
-  vim.keymap.set('n', 'ö', function()
-    vim.api.nvim_feedkeys(']', 'i', false)
-  end, { expr = true, desc = '"]"' })
-
-  vim.keymap.set('n', 'Ö', function()
-    vim.api.nvim_feedkeys('[', 'i', false)
-  end, { expr = true, desc = '"]"' })
+  require'toggle_keys'.initKeyMaps()
 
   -- Document existing key chains
   wk.add {
--- lua/toggle_keys.lua
+++ lua/toggle_keys.lua
@@ -6,5 +6,34 @@ M.toggle_keys = function()
   M.coding = not M.coding
 end
 
+M.initKeyMaps = function()
+  vim.keymap.set('i', 'ö', function()
+    return require('toggle_keys').coding and '[' or 'ö'
+  end, { expr = true, desc = 'coding "["' })
+
+  vim.keymap.set('i', 'Ö', function()
+    return require('toggle_keys').coding and '{' or 'Ö'
+  end, { expr = true, desc = 'coding "{"' })
+
+  vim.keymap.set('i', 'ä', function()
+    return require('toggle_keys').coding and ']' or 'ä'
+  end, { expr = true, desc = 'coding "]"' })
+
+  vim.keymap.set('i', 'Ä', function()
+    return require('toggle_keys').coding and '}' or 'Ä'
+  end, { expr = true, desc = 'coding "}"' })
+
+  vim.keymap.set('n', 'ö', function()
+    vim.api.nvim_feedkeys(']', 'i', false)
+  end, { expr = true, desc = '"]"' })
+
+  vim.keymap.set('n', 'ä', function()
+    vim.api.nvim_feedkeys('[', 'i', false)
+  end, { expr = true, desc = '"]"' })
+
+  vim.keymap.set('n', '<leader>t', function()
+    require('toggle_keys').toggle_keys()
+  end, { desc = 'toggle between coding and german writing keys' })
+end
 
 return M
